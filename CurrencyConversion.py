83a41295f8fbda36d9c0474629cfb636677d10e6
KalinKrastev313 <93783199+KalinKrastev313@users.noreply.github.com>
2024-06-07 13:44:04 +0300

    Currency input functions

--- CurrencyConversion.py
+++ CurrencyConversion.py
@@ -1,14 +1,44 @@
 import sys
 from datetime import datetime
 
+from constants import currency_codes
+
+
+def receive_amount_and_validate():
+    while True:
+        amount = input()
+        try:
+            value = float(amount)
+
+            if round(value, 2) == value:
+                return value
+            else:
+                print("Please enter a valid amount")
+        except ValueError:
+            print("Please enter a valid amount")
+
+
+def receive_currency_and_validate():
+    while True:
+        curr_code = input()
+        # The first check for length is executed first and thus saves time
+        if len(curr_code) == 3 and curr_code.upper() in currency_codes:
+            return curr_code.upper()
+        else:
+            print('Please enter a valid currency code')
+
 
 def main(date_str):
     try:
         date = datetime.strptime(date_str, '%Y-%m-%d')
         print(f"Performing currency conversion for the date: {date.strftime('%Y-%m-%d')}")
 
-        print("Currency conversion logic goes here.")
+        amount = receive_amount_and_validate()
+        base_currency = receive_currency_and_validate()
+        target_currency = receive_currency_and_validate()
 
+        print(base_currency)
+        print(target_currency)
     except ValueError:
         print("Error: The date format should be YYYY-MM-DD.")
         sys.exit(1)
